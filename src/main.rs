a46fb2e96f9895245ff98e37adf2877efa4ae5df
S.&'hi.S.M <91532714+SandhiSM@users.noreply.github.com>
2023-01-28 17:11:56 +0900

    Fix this program to work

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,52 @@ fn main() {
     let instance = construct();
     let result = instance.clone().calculate();
     let recalculate = || {
+        let alphabet_to_number = |numbers: &Vec<String>| {
+            const ALPHABET: [&str; 26] = [
+                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
+                "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+            ];
+            let alphabet_convert_table = hashmap!["A"=> "10", "B"=> "11", "C"=> "12", "D"=> "13", "E"=> "14", "F"=> "15", "G"=> "16", "H"=> "17", "I"=> "18", "J"=> "19", "K"=> "20", "L"=> "21", "M"=> "22", "N"=> "23", "O"=> "24", "P"=> "25", "Q"=> "26", "R"=> "27", "S"=> "28", "T"=> "29", "U"=> "30", "V"=> "31", "W"=> "32", "X"=> "33", "Y"=> "34", "Z"=> "35"];
+            let mut number = Vec::<String>::new();
+            'first: for character in numbers {
+                for alphabet in ALPHABET {
+                    if character == alphabet {
+                        number.push(alphabet_convert_table[&character.to_string()].clone());
+                        continue 'first;
+                    } else {
+                        continue;
+                    }
+                }
+                number.push(character.to_string());
+            }
+            number
+        };
+        let number_to_alphabet = |numbers: Vec<String>| {
+            let mut output = Vec::<String>::new();
+            let alphabet_convert_table = hashmap!["10"=> "A", "11"=> "B", "12"=> "C", "13"=> "D", "14"=> "E", "15"=> "F", "16"=> "G", "17"=> "H", "18"=> "I", "19"=> "J", "20"=> "K", "21"=> "L", "22"=> "M", "23"=> "N", "24"=> "O", "25"=> "P", "26"=> "Q", "27"=> "R", "28"=> "S", "29"=> "T", "30"=> "U", "31"=> "V", "32"=> "W", "33"=> "X", "34"=> "Y", "35"=> "Z"];
+            'first: for number in numbers {
+                for (figure, alphabet) in alphabet_convert_table.clone() {
+                    if number == figure {
+                        output.push(alphabet);
+                        continue 'first;
+                    } else {
+                        ()
+                    }
+                }
+                output.push(number);
+            }
+            return output;
+        };
         let confirmation = Number {
-            number: result.clone(),
+            number: alphabet_to_number(&result),
             from: instance.to,
             to: instance.from,
             minus: instance.minus,
             is_integer: instance.is_integer,
         }
         .calculate();
-        if instance.number == confirmation {
+        let number = number_to_alphabet(instance.number);
+        if number == confirmation {
             return true;
         } else {
             return false;
@@ -78,7 +115,7 @@ fn construct() -> Number {
         ];
         let alphabet_convert_table = hashmap!["A"=> "10", "B"=> "11", "C"=> "12", "D"=> "13", "E"=> "14", "F"=> "15", "G"=> "16", "H"=> "17", "I"=> "18", "J"=> "19", "K"=> "20", "L"=> "21", "M"=> "22", "N"=> "23", "O"=> "24", "P"=> "25", "Q"=> "26", "R"=> "27", "S"=> "28", "T"=> "29", "U"=> "30", "V"=> "31", "W"=> "32", "X"=> "33", "Y"=> "34", "Z"=> "35"];
         let mut number = Vec::<String>::new();
-        'first: for character in arguments[2].chars() {
+        'first: for character in arguments[1].chars() {
             for alphabet in ALPHABET {
                 if character == alphabet {
                     number.push(alphabet_convert_table[&character.to_string()].clone());
@@ -107,9 +144,9 @@ fn construct() -> Number {
             return (0, 0);
         }
     };
-    let minus_checker = move |number: Vec<String>| {
+    let minus_checker = |number: Vec<String>| {
         let mut numbers = number;
-        if numbers[0] == "." {
+        if numbers[0] == "-" {
             numbers.remove(0);
             return (numbers, true);
         } else {
@@ -156,7 +193,7 @@ fn construct() -> Number {
     if condition_checker(&arguments) {
         let number = alphabet_to_number(&arguments);
         let (from, to) = range_checker(&arguments);
-        if from == 0 {
+        if from != 0 {
             let (number, minus) = minus_checker(number);
             let integer = is_integer(&arguments);
             if number_checker(&number, from, integer) {
@@ -197,15 +234,13 @@ impl Number {
             };
             let mut length = self.number.len() as u32;
             let numbers = convert_to_u128();
-            let mut converted_number = 0;
-            let mut old_number = 0;
+            let mut new_number = 0;
             for number in numbers {
-                converted_number = (self.from as u128).pow(length - 1) * number;
-                let new_number = converted_number + old_number;
-                old_number = new_number;
+                let converted_number = (self.from as u128).pow(length - 1) * number;
+                new_number = converted_number + new_number;
                 length -= 1;
             }
-            return converted_number;
+            return new_number;
         };
         let convert_to_dec_float = || {
             let convert_to_f64 = || {
@@ -229,17 +264,15 @@ impl Number {
                 return (converted_number, dot_index as i32);
             };
             let (numbers, mut length) = convert_to_f64();
-            let mut converted_number = 0.0;
-            let mut old_number = 0.0;
+            let mut new_number = 0.0;
             for number in numbers {
-                converted_number = (self.from as f64).powi(length - 1) * number;
-                let new_number = converted_number + old_number;
-                old_number = new_number;
+                let converted_number = (self.from as f64).powi(length - 1) * number;
+                new_number = converted_number + new_number;
                 length -= 1;
             }
-            return converted_number;
+            return new_number;
         };
-        let number_to_alphabet = move |number: u128| {
+        let number_to_alphabet = |number: u128| {
             let number = number.to_string();
             let alphabet_convert_table = hashmap!["10"=> "A", "11"=> "B", "12"=> "C", "13"=> "D", "14"=> "E", "15"=> "F", "16"=> "G", "17"=> "H", "18"=> "I", "19"=> "J", "20"=> "K", "21"=> "L", "22"=> "M", "23"=> "N", "24"=> "O", "25"=> "P", "26"=> "Q", "27"=> "R", "28"=> "S", "29"=> "T", "30"=> "U", "31"=> "V", "32"=> "W", "33"=> "X", "34"=> "Y", "35"=> "Z"];
             for (figure, alphabet) in alphabet_convert_table {
@@ -294,8 +327,10 @@ impl Number {
             let integer = integer as u128;
             if integer >= 1 {
                 output = int_calculate(integer);
+                output.push(".".to_string());
             } else {
-                ()
+                output.push("0".to_string());
+                output.push(".".to_string());
             }
             while float != 0.0 {
                 let decimal = float * self.to as f64;
@@ -305,10 +340,15 @@ impl Number {
                 } else {
                     ()
                 }
-                output.push((integer as u128).to_string());
+                if integer >= 10.0 {
+                    let integer = number_to_alphabet(integer as u128);
+                    output.push(integer);
+                } else {
+                    output.push((integer as u128).to_string());
+                }
                 float = decimal - integer;
             }
-            if output[output.len()] == ".".to_string() {
+            if output[output.len() - 1] == ".".to_string() {
                 output.pop();
             } else {
                 ()
@@ -336,6 +376,7 @@ fn finalize(vector: Vec<String>) -> String {
 fn input_y_or_n() -> String {
     let mut y_or_n = String::new();
     std::io::stdin().read_line(&mut y_or_n).unwrap();
+    y_or_n = y_or_n.replace("\r\n", "");
     if y_or_n == "y".to_string() || y_or_n == "n".to_string() {
         return y_or_n;
     } else {
